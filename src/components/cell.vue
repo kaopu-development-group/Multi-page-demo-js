adde553b8d53280f8fe7cba6657b13ff5c63761a
yijinghao <yjinhao@qq.com>
2019-05-26 17:41:21 +0800

    demo

---
+++ src/components/cell.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hello">
+      <a href="index.html">index</a>
+    <a href="cell.html">cell.html</a>
+        <h1>{{ msg }}</h1>
+        <p>你为什么不用话</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'cell',
+  data () {
+    return {
+      msg: '多页面测试'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    sdasdasd
+    <!-- <router-view></router-view> -->
+    <Hello></Hello>
+
+    <Index-part></Index-part>
+  </div>
+</template>
+
+
+<script>
+import Hello from '@/components/Hello'
+import IndexPart from '@/components/IndexPart'
+export default {
+  name: 'app',
+  data:{
+    indexData:{
+      name:"yjh"
+    }
+  },
+  components: { 
+    Hello ,
+    IndexPart
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
